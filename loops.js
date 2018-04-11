f9a36db9c8a8364eda48f801ff1919982d729205
Bethany Oestrich <bethany@oestrich.org>
2018-04-11 00:29:48 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -19,7 +19,7 @@ function whileLoop(number){
 }
 function doWhileLoop (array){
   function maybeTrue(){
-    return Math.random()
+    return Math.random()>=0.5
   }
   do {
     console.log ('doo-bee-doo-bee-doo')
